69cd9d5070a8fc748eac3a2a720f976791664757
Allenisanoob <0522allen@gmail.com>
2024-04-20 00:30:47 +0800

    Coded Held-Karp Algorithm for TSP in algorithm.py using recursion, works for 5x5 map but not 6x8. Need to implement memoization to make it work.

--- python/Route_Algorithm/Algorithm.py
+++ python/Route_Algorithm/Algorithm.py
@@ -1,13 +1,29 @@
 from map import *
 
-def get_total_operations() -> deque:
+#Held-Karp Algorithm
+def g(S:set, e:int) -> int:
+    print(S, e)
+    if len(S) == 0:
+        return 0
+    else:
+        return min([g(S - {j}, j) + all_paths[j][e][2] for j in S])
+
+def get_total_operations() -> int:
     raw_map = read_csv('python\\Route_Algorithm\\maze.csv')
-    maze = Map(raw_map, 8, 6)
-    endnode_num = len(maze.score_points)
-    distance = [[0] * endnode_num] * endnode_num
-    for i in range(endnode_num):
-        for j in range(endnode_num):
-            distance[i][j] = maze.get_distance(maze.score_points[i], maze.score_points[j])
+    maze = Map(raw_map, 6, 8, starting_point_index = 1)
+    SP_num = len(maze.score_points)
+    
+    global all_paths
+    all_paths = [[0] * SP_num] * SP_num
+    for i in range(SP_num):
+        for j in range(SP_num):
+            all_paths[i][j] = maze.path_find(maze.score_points[i].index, maze.score_points[j].index)
+    
+    #Held-Karp Algorithm
+    S = set(range(SP_num))
+    return g(S - {0}, 0)
+
 
 if __name__ == "__main__":
-    get_total_operations()
+    operations = get_total_operations()
+    print(operations)
