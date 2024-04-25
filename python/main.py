bfd09743c3f5a6cb09c66f7f445d8dce9e81787b
Allenisanoob <0522allen@gmail.com>
2024-04-25 19:31:45 +0800

    main fix

--- python/main.py
+++ python/main.py
@@ -14,7 +14,6 @@ def main():
     operations = ag.get_total_operations()
     preview = ag.get_best_route()
     m_buffer = bytes()
-    route, num = 0, 0
 
     log = logging.getLogger("scoreboard")
     logging.basicConfig(level=logging.DEBUG)
@@ -40,7 +39,6 @@ def main():
                 dir = "l" if dir == "L" else dir
                 print(dir)
                 bti1.bt.serial_write_bytes(dir.encode("utf-8"))
-                num += 1
                 m_buffer = m_buffer[4:]
                 print("Next State")
             elif m_buffer[1] == 2:
@@ -49,25 +47,19 @@ def main():
                 score, time_remaining = scoreboard.add_UID(uid[2:])
                 current_score = scoreboard.get_current_score()
                 log.info(f"Current score: {current_score}")
-                num = 0
-                route += 1
                 m_buffer = m_buffer[8:]
                 print(uid)
 
             elif m_buffer[1] == 3:
-                dir=operations.popleft()
-                if dir=='r':
-                    fixdir='l'
+                dir = operations.popleft()
+                if dir == 'r':
+                    fixdir = 'l'
                     bti1.bt.serial_write_bytes(fixdir.encode("utf-8"))
-                elif dir=='L':
-                    fixdir='f'
+                elif dir == 'L' or (dir == 'l' and operations[0] == 'l'):
+                    fixdir = operations.popleft()
                     bti1.bt.serial_write_bytes(fixdir.encode("utf-8"))
-                elif dir=='l' and operations[0]=='l':
-                    operations.popleft()
-                    fixdir='f'
-                    bti1.bt.serial_write_bytes(fixdir.encode("utf-8"))
-                elif dir=='l':
-                    fixdir='r'
+                elif dir == 'l':
+                    fixdir = 'r'
                     bti1.bt.serial_write_bytes(fixdir.encode("utf-8"))
 
 if __name__ == "__main__":
